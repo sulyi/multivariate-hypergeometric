b6be0db110f0614a99258c2e97d26775c7fec9c2
sulyi <sulyi.gbox@gmail.com>
2014-11-14 13:00:39 +0100

    I like my spaces

--- mvhgd/core/grid.py
+++ mvhgd/core/grid.py
@@ -2,25 +2,26 @@ from draw import Draw
 from level import Level
 
 
-class Grid(object):
+class Grid ( object ):
+
     """
     """
     # TODO: doc, Grid
 
-    __slots__ = ['root', 'm', 'roof', '_default', '_len_tab', '_iroot']
+    __slots__ = [ 'root', 'm', 'roof', '_default', '_len_tab', '_iroot' ]
 
-    def __init__(self, supremum=[]):
-        self.root = Draw(supremum)
-        self.m = len(supremum)
-        self.roof = sum(self.root)
-        self._len_tab = [[1] for _ in range(self.m)]
-        self._iroot = [sum(self.root[:i]) for i in range(self.m)]
+    def __init__( self, supremum=[] ):
+        self.root = Draw( supremum )
+        self.m = len( supremum )
+        self.roof = sum( self.root )
+        self._len_tab = [ [1] for _ in range(self.m) ]
+        self._iroot = [ sum(self.root[:i]) for i in range(self.m) ]
         self._default = self.limit_to()
 
-    def __repr__(self):
+    def __repr__( self ):
         return "Grid(%r)" % self.root
 
-    def __str__(self):
+    def __str__( self ):
         return "Grid instance with supremum of %s" % self.root
 
     def __iter__(self):
@@ -28,13 +29,13 @@ class Grid(object):
 
     # Python 3 compatibility
     def __next__(self):
-        return next(self._default)
+        return next( self._default )
 
     # Python 2 compatibility
     def next(self):
-        return next(self._default)
+        return next( self._default )
 
-    def limit_to(self, target=None):
+    def limit_to( self, target=None ):
 
         """
         """
@@ -59,15 +60,14 @@ class Grid(object):
         else:
             floor = 0
             target = [0] * self.m
-
-        previous = Level(self, [self.root])
+        previous = Level( self, [ self.root ] )
         yield previous
 
-        for n in range(self.roof, floor, -1):
-            previous = previous.next_level(n, target)
+        for n in range( self.roof, floor, -1 ):
+            previous = previous.next_level( n, target )
             yield previous
 
-    def _read_len_tab(self, n, i):
+    def _read_len_tab( self, n, i ):
         # unsafe, since _len_tab is filled during generation
         n_ast = n - self._iroot[i]
         return self._len_tab[i][n_ast] if n_ast >= 0 else 0
